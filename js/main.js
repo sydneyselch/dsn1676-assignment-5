31ffe38bd1fbd0ffd72556532f90ee36b0623862
Sydney Selch <sydney_selch@Sydneys-MacBook-Pro-4.local>
2015-02-26 10:52:34 -0500

    Li added and working with X

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ $form.on('submit', function ($eventObject){
     var $theLi = $('<li>');
     $theLi.html($input.val());
      
-     $list.append('<li>' + $input + '<a href="">x</a>')
+     $list.append('<li>' + $input.val() + '<a href="">x</a></li>' )
      
      $input.val('');
     
@@ -20,18 +20,3 @@ $list.on('click', 'a', function (e) {
 });
 
     
-
-$('form').submit(function () {
-    if ($('input').val() !== '') {
-        var input_value = $('input').val();
-        $('ul').append('<li>' + input_value + '<a href="">x</a></li>');
-
-    };
-    $('input').val('');
-    return false;
-});
-
-$(document).on('click', 'a', function (e) {
-    e.preventDefault();
-    $(this).parent().remove();
-});
