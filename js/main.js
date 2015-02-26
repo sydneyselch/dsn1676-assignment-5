a7028d42e680baefda57b3b1924c4575b72e3f13
Sydney Selch <sydney_selch@Sydneys-MacBook-Pro-4.local>
2015-02-26 10:44:25 -0500

    x added and removal working

--- js/main.js
+++ js/main.js
@@ -8,9 +8,30 @@ $form.on('submit', function ($eventObject){
     var $theLi = $('<li>');
     $theLi.html($input.val());
      
-     $list.append($theLi)
+     $list.append('<li>' + $input + '<a href="">x</a>')
      
      $input.val('');
     
 });
 
+$list.on('click', 'a', function (e) {
+    e.preventDefault();
+    $(this).parent().remove();
+});
+
+    
+
+$('form').submit(function () {
+    if ($('input').val() !== '') {
+        var input_value = $('input').val();
+        $('ul').append('<li>' + input_value + '<a href="">x</a></li>');
+
+    };
+    $('input').val('');
+    return false;
+});
+
+$(document).on('click', 'a', function (e) {
+    e.preventDefault();
+    $(this).parent().remove();
+});
